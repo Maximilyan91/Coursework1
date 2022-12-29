75d9ef47e02b96f9d40ef8d23f59d0315f1f924d
Максим <gmax7911@gmail.com>
2022-12-29 15:53:58 +0300

    Начало работы

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,21 @@
 public class Employee {
+    String fullName;
+    int department;
+    int salary;
+    int id;
+    int idCount;
 
 
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
