784c4334518b63d1c507377ecee1af3384339b11
Максим <gmax7911@gmail.com>
2023-01-03 10:31:47 +0300

    в процессе сложный уровень

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,25 @@ public class Main {
     public static void main(String[] args) {
         Coursework.printEmployees();
         System.out.println("Сумма расходов на зарплату сотрудникам - " + Coursework.calculateSalaryCosts());
+        System.out.println();
         System.out.println("Cотрудник с минимальной зарплатой - " + Coursework.findMinSalaryEmployer());
+        System.out.println();
         System.out.println("Сотрудник с максимальной зарплатой - " + Coursework.findMaxSalaryEmployer());
+        System.out.println();
         System.out.println("Средняя сумма зарплаты - " + Coursework.findAverageValue());
+        System.out.println();
         Coursework.printAllNames();
+        System.out.println();
+        Coursework.findIndexSalary(18);
+        System.out.println();
 
+        System.out.println("Сотрудник с минмиальной зарплатой в отделе - " + Coursework.findMinSalaryEmployerDepartment(4));
+        System.out.println();
+        System.out.println("Сотрудник с максимальной зарплатой в отделе - " + Coursework.findMaxSalaryEmployerDepartment(4));
+        System.out.println();
+        System.out.println("Сумма зарплат по отделу - " + Coursework.calculateSalaryCostsInDepartment(2));
+        System.out.println();
+        System.out.println("Средняя зарплата по отделу - " + Coursework.findAverageValueInDepartment(1));
     }
+
 }
